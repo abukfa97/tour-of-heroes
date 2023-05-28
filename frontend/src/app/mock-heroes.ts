ba1f53cd0dcfa3cd9135b401909a399205c97c4a
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-05-28 16:25:17 +0200

    Create mocked heroes

---
+++ frontend/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+  {id: 1, name: 'Ironman'},
+  {id: 2, name: 'Superman'},
+  {id: 3, name: 'Spiderman'},
+  {id: 4, name: 'Batman'},
+  {id: 5, name: 'Flash'},
+  {id: 6, name: 'Ant'},
+  {id: 7, name: 'Catwoman'},
+  {id: 8, name: 'Punisher'},
+  {id: 9, name: 'Venom'},
+];
