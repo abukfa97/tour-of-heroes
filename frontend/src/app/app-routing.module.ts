830892bd85b01c4a882cd9fc798fbc17b58dcda0
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-06-02 23:39:26 +0200

    Add detail component to the routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import {HeroesComponent} from "./heroes/heroes.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent},
+  { path: '', component: DashboardComponent, pathMatch: "full"},
   { path: 'dashboard', component: DashboardComponent},
-  { path: '', component: DashboardComponent, pathMatch: "full"}
+  { path: 'heroes', component: HeroesComponent},
+  { path: 'detail/:id', component:HeroDetailComponent},
 ];
 
 @NgModule({
