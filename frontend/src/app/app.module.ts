03efb95566462a22dd7627ecca360fa071483580
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-06-03 01:16:49 +0200

    Configure InMemoryWebApiModule with InMemoryDataService

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {HttpClientModule} from "@angular/common/http";
+import { InMemoryDataService } from "./in-memory-data.service";
+import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 
 @NgModule({
   declarations: [
@@ -22,7 +24,10 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
