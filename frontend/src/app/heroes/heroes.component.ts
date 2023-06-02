340a245f6c5df57c5e91b3a09c1076a84517ebad
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-06-02 23:47:45 +0200

    Refactoring heroes component

--- frontend/src/app/heroes/heroes.component.ts
+++ frontend/src/app/heroes/heroes.component.ts
@@ -9,19 +9,14 @@ import {MessageService} from "../message.service";
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes)
-  }
-
-  onSelect(hero: Hero) : void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`);
   }
 
   ngOnInit():void{
